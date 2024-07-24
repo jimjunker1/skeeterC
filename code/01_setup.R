9bdc1cf78af1c1736796dc67d6909034e38c84ed
Jim Junker <james.junker1@gmail.com>
2024-07-24 15:29:35 -0500

    do data cleaning script

---
+++ code/01_setup.R
@@ -0,0 +1,37 @@
+here::i_am("code/01_setup.R")
+#script to setup project details
+
+## load packages
+library(tidyverse)
+library(here)
+
+## define user functions
+#' @title read_miniDOT
+#' @Description This function reads in data from the output of PME miniDOT logger software
+#' @param x x is the filepath to the TXT file exported from miniDOTconcatenate program
+#'
+read_miniDOT = function(x, list = FALSE,...){
+  metadata = readLines(x, n = 6)
+  labels = trimws(read.table(x, header = FALSE, sep = ",", nrows = 1, skip = 7))
+  units = trimws(read.table(x, header = FALSE, sep = ",", nrows = 1, skip = 8))
+  data = read.table(x, header = FALSE, sep = ",", skip = 9, strip.white = TRUE)
+  colNames = paste(labels, units)
+  colnames(data) <- colNames
+  data[["UTC_Date_&_Time (none)"]] <- as.POSIXct(data[["UTC_Date_&_Time (none)"]], format = "%Y-%m-%d %H:%M:%S")
+  data[["Central Standard Time (none)"]] <- as.POSIXct(data[["Central Standard Time (none)"]], format = "%Y-%m-%d %H:%M:%S")
+  if(list){
+    return(list(metadata = metadata,
+                data = data))
+  } else{return(data)}
+}
+## set options
+options(mc.cores = parallel::detectCores())
+
+## user setting
+raft_filepath = sprintf("C:/Users/%s/OneDrive - UNT System/AERI Seed Insect Carbon/Data/2024_aeri_seed_mosquito_data.xlsx", Sys.info()[['user']])
+
+pred_filepath = sprintf("C:/Users/%s/OneDrive - UNT System/Shared Documents - Leaf Litter Hot Moments/General/2024_predictions_aeri_seed_mosquito.xlsx", Sys.info()[['user']])
+
+metab_meta_filepath = sprintf("C:/Users/%s/OneDrive - UNT System/AERI Seed Insect Carbon/Data/2024_aeri_seed_logger-meta.xlsx", Sys.info()[['user']])
+metab_depth_filepath = sprintf("C:/Users/%s/OneDrive - UNT System/AERI Seed Insect Carbon/Data/2024_aeri_seed_water-depth_data.xlsx", Sys.info()[['user']])
+metab_folderpath = sprintf("C:/Users/%s/OneDrive - UNT System/AERI Seed Insect Carbon/Data/DO logger data/", Sys.info()[['user']])
---
+++ code/05_metabolism.R
@@ -0,0 +1,39 @@
+# initiate set ups
+source(here::here("code/01_setup.R"))
+i_am("code/05_metabolism.R")
+
+library(LakeMetabolizer)
+
+## read in the logger metadata
+logger_meta <- readxl::read_excel(metab_meta_filepath, col_types = c('text',
+                                                                     'numeric',
+                                                                     'text',
+                                                                     'text',
+                                                                     'text',
+                                                                     'numeric',
+                                                                     'date',
+                                                                     'date',
+                                                                     'date',
+                                                                     'date',
+                                                                     'text')) %>%
+  rename_with(tolower) %>%
+  mutate(`deploy time` = gsub("\\d{4}-\\d{2}-\\d{2} (\\d{2}\\:\\d{2}\\:\\d{2})", "\\1", `deploy time`),
+         `collection time` = gsub("\\d{4}-\\d{2}-\\d{2} (\\d{2}\\:\\d{2}\\:\\d{2})", "\\1", `collection time`)) %>%
+  unite("deploy", `deploy date`:`deploy time`, sep = " ", remove = TRUE) %>%
+  unite("collection", `collection date`:`collection time`, sep = " ", remove = TRUE) %>%
+  mutate(deploy = as.POSIXct(deploy, format = '%Y-%m-%d %H:%M:%S'),
+         collection = as.POSIXct(collection, format = '%Y-%m-%d %H:%M:%S'),
+         `logger id` = as.character(`logger id`))
+
+# read in the logger data
+logger_filepath = list.files(metab_folderpath, all.files = TRUE, recursive = TRUE, full.names = TRUE)
+logger_names = gsub(".*/DO logger data/(\\d{6}).TXT","\\1",logger_filepath)
+
+logger_df = logger_filepath%>%
+  map(\(txt) read_miniDOT(txt, list = FALSE)) %>%
+  setNames(nm = logger_names) %>%
+  bind_rows(.id = "loggerID") %>%
+  left_join(logger_meta %>% select(loggerID = 'logger id', block, mesocosm, treatment),., by = "loggerID")
+
+
+
