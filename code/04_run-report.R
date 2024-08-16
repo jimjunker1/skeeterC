1ac54070daddd98d69315981d2cfb34c78a4551c
Jim Junker <james.junker1@gmail.com>
2024-08-16 13:23:55 -0500

    updated final forecast.

--- code/04_run-report.R
+++ code/04_run-report.R
@@ -1,6 +1,6 @@
 # script to run daily report
 
-rmarkdown::render(here::here("R/skeeter-forecast-template.Rmd"),
+rmarkdown::render(here::here("code/skeeter-forecast-template.Rmd"),
                   output_format = "pdf_document",
                   output_dir = here::here("forecasts/"),
                   output_file = paste0(Sys.Date(),"_skeeter-forecast.pdf"),
